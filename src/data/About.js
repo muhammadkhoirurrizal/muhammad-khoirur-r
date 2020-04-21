16590b76e4ad4a88ea5117904b88cbea639a2e78
KhoirurRizal <rizalkhoirur96@gmail.com>
2020-04-21 16:37:42 +0700

    Typo

--- src/data/About.js
+++ src/data/About.js
@@ -3,14 +3,14 @@ export default [
     id: 1,
     title: "Experience",
     description:
-      "Until now i work as a game progammer and font end developer. I have a good experience in doing my profession.",
-    path: "/experience"
+      "Until now i work as a game progammer and front end developer. I have a good experience in doing my profession.",
+    path: "/experience",
   },
   {
     id: 2,
     title: "Projects",
     description:
       "I like to constantly learn and grow with different projects. Here’s a comprehensive list of projects I’ve worked on. ",
-    path: "/projects"
-  }
+    path: "/projects",
+  },
 ];
--- src/data/Work.js
+++ src/data/Work.js
@@ -10,7 +10,7 @@ export default [
     logo: twiscode,
     companyLink: "https://twiscode.com/",
     description:
-      "<p>Twiscode was founded in 2014 to serve website development needs in Indonesia & Singapore. As the team has grown from 5 people to 50 people in 2020, the company also expanded its development team to Android & iOS Applications as well.</p><p>Contribution : <p> <ul><li>Responsible develop landing page Twiscode.</li><li>Responsible develop dasboard admin GPBB.</li><li>Responsible develop landing page Halobro.</li><li>Responsible develop landing page A&D Entertainment.</li><li>Responsible develop Wiku - Wiku website.</li><li>Responsible develop manga website Mangajek.</li></ul></p></p>"
+      "<p>Twiscode was founded in 2014 to serve website development needs in Indonesia & Singapore. As the team has grown from 5 people to 50 people in 2020, the company also expanded its development team to Android & iOS Applications as well.</p><p>Contribution : <p> <ul><li>Responsible develop landing page Twiscode.</li><li>Responsible develop dasboard admin GPBB.</li><li>Responsible develop landing page Halobro.</li><li>Responsible develop landing page A&D Entertainment.</li><li>Responsible develop Wiku - Wiku website.</li><li>Responsible develop manga website Mangajek.</li></ul></p></p>",
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ export default [
     logo: otebe,
     companyLink: "",
     description:
-      "<p>OTEBE Smart is educational technology development company that hope to become best innovation company.</p><p>Contribution : <p> <ul><li>Responsible for development game education using technology AR ( Augmented Reality ) for children.</li><li>Developed several game using technology VR ( Virtual Reality ).</li></ul></p></p>"
+      "<p>OTEBE Smart is educational technology development company that hope to become best innovation company.</p><p>Contribution : <p> <ul><li>Responsible for development game education using technology AR ( Augmented Reality ) for children.</li><li>Developed several game using technology VR ( Virtual Reality ).</li></ul></p></p>",
   },
   {
     id: 1,
@@ -30,6 +30,6 @@ export default [
     logo: "",
     companyLink: "",
     description:
-      "<p>Develope saveral game education that based on Unity Engine.</p>"
-  }
+      "<p>Develop several game education that based on Unity Engine.</p>",
+  },
 ];
